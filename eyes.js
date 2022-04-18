7902f3099c19e00d7b0b2f7d4d183702ef885bbc
H0gs <95542282+H0gs@users.noreply.github.com>
2022-04-18 16:49:20 -0400

    Add files via upload

--- eyes.js
+++ eyes.js
@@ -1,14 +1,24 @@
 const balls = document.getElementsByClassName('ball');
-
+console.log("H")
+console.log(balls);
 document.onmousemove = (event) => {
+    // console.log("Moved!")
+    // console.log("DOne")
   const x = (event.clientX * 100) / window.innerWidth + '%';
   const y = (event.clientY * 100) / window.innerHeight + '%';
+  // console.log("DOne1")
 
-
-  for (let i = 0; i < 2, i++){
+  for (let i = 0; i < balls.length; i++){
+      // console.log("Hi");
       balls[i].style.left = x;
       balls[i].style.top = y;
       balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+      // document.appendChild(balls[i]);
+      // console.log(x);
+      // console.log(y);
+      
   }
-
+  console.log(x);
+  console.log(y);
+  // console.log("DOne2")
 };
